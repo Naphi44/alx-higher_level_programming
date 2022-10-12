9d2993dfef4e71a8321002cf43880924d832e13a
Naphi <78766238+Naphi44@users.noreply.github.com>
2022-10-12 10:30:42 +0300

    Relation

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
-"""This script lists all State objects from the database hbtn_0e_6_usa
-   Usage:  ./11-model_state_insert.py   <mysql username> /
-                                        <mysql password> /
-                                        <database name>
+"""
+This script prints all City objects
+from the database `hbtn_0e_14_usa`.
 """
 
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State, Base
-from sys import argv
 
 if __name__ == "__main__":
-    if len(argv) != 4:
-        print("Usage: {} username password database".format(argv[0]))
-        exit(1)
+    """
+    Access to the database and get the cities
+    from the database.
+    """
 
-    username, password, db = argv[1], argv[2], argv[3]
-
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(username, password, db),
-        pool_pre_ping=True)
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
+
     session = Session()
-    newstate = State(name="Louisiana")
-    session.add(newstate)
+    cal_state = State(name='California')
+    sfr_city = City(name='San Francisco')
+    cal_state.cities.append(sfr_city)
+
+    session.add(cal_state)
     session.commit()
-    print(newstate.id)
+    session.close()
